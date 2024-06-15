8f89109840e9ed11fa35928f1ea627e8fb72e4ae
Shikder Tarek <shikdertarek2019@gmail.com>
2024-06-15 23:07:58 +0600

    Add sort and search option on available camps

--- src/Pages/AvailableCamps/AvailableCamps.jsx
+++ src/Pages/AvailableCamps/AvailableCamps.jsx
@@ -1,41 +1,78 @@
-import { useEffect } from "react";
-import { FaLocationArrow } from "react-icons/fa";
-import { FaLocationCrosshairs, FaLocationPin, FaLocationPinLock, FaMapLocation, FaPeopleGroup } from "react-icons/fa6";
+
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import {FaLocationPin, FaPeopleGroup } from "react-icons/fa6";
 import { useLoaderData } from "react-router-dom";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
 
 
 const AvailableCamps = () => {
-    const camps = useLoaderData()   
+    // const camps = useLoaderData() 
+
+    
+    const [search, setSearch] = useState("");
+    const [sort, setSort] = useState("");
+    const [camps, setCamps] = useState([])
+    const axiosPublic = useAxiosPublic()
+
+    useEffect(()=>{
+        const queryParams = new URLSearchParams({ search, sort }).toString();
+        axiosPublic.get(`/camps?${queryParams}`)
+        .then(res=>{
+            console.log(res.data);
+            setCamps(res.data)
+        })
+    },[axiosPublic, search, sort])
+
+
 
     return (
         <div>
-            <h2 className="text-2xl font-bold text-center">Available Camps</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-                {
-                    camps.map(camp => <div key={camp._id} className="card card-compact bg-base-100 shadow-xl">
-                        <figure><img className="h-72 w-full object-cover" src={camp.image} alt="Shoes" /></figure>
-
-                        <div className="card-body">
-                            <div className="flex justify-between w-auto">
+        <h2 className="text-2xl font-bold text-center mt-8">Available Camps</h2>
+        <div className="mb-8 flex flex-col md:flex-row md:justify-evenly items-center">
+            <input
+                type="text"
+                placeholder="Search camps"
+                className="input input-bordered mb-4 md:mb-0"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
+            <select
+                className="select select-bordered"
+                value={sort}
+                onChange={(e) => setSort(e.target.value)}
+            >
+                <option value="">Sort Available Camp</option>
+                <option value="mostRegistered">Most Registered</option>
+                <option value="campFees">Camp Fees</option>
+                <option value="alphabeticalOrder">Alphabetical Order</option>
+            </select>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+            {camps.map(camp => (
+                <div key={camp._id} className="card card-compact bg-base-100 shadow-xl">
+                    <figure><img className="h-72 w-full object-cover" src={camp.image} alt="Camp" /></figure>
+                    <div className="card-body">
+                        <div className="flex justify-between w-auto">
                             <p>{new Date(camp.dateTime).toLocaleDateString()}</p>
                             <p className="flex gap-2 items-center">
-                               <FaLocationPin/> {camp.location}
+                                <FaLocationPin /> {camp.location}
                             </p>
-                            </div>
-                          <div className="flex justify-between">
-                          <h2 className="card-title">{camp.campName}</h2>
-                          <p className="flex items-center gap-2"><FaPeopleGroup /> {camp.participantCount}</p>
-                          </div>
-                          <h3 className="font-bold text-base">Doctor: {camp.healthcareProfessionalName}</h3>
-                          <p>{camp.description}</p>
-                          <div className="card-actions justify-end">
-                            <button className="btn btn-primary">Buy Now</button>
-                          </div>
                         </div>
-                      </div>)
-                }
-            </div>
+                        <h2 className="card-title">{camp.campName}</h2>
+                        <div className="flex justify-between">
+                            <h3 className="font-bold text-base">Doctor: {camp.healthcareProfessionalName}</h3>
+                            <p className="flex items-center justify-end gap-2"><FaPeopleGroup /> {camp.participantCount}</p>
+                        </div>
+                        <p>{camp.description}</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+            ))}
         </div>
+    </div>
     );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,23 @@ import {
 import router from './Routes/Routes.jsx';
 import AuthProvider, { AuthContext } from './Pages/Providers/AuthProvider.jsx';
 import { HelmetProvider } from 'react-helmet-async';
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
+     <QueryClientProvider client={queryClient}>
+     <HelmetProvider>
     <AuthProvider>
     <RouterProvider router={router} />
     </AuthProvider>
     </HelmetProvider>
+    </QueryClientProvider>
+    
   </React.StrictMode>,
 )
