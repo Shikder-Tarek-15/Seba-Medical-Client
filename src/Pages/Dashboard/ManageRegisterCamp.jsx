404def4dc81220ccf5b0789a46453d51039a2726
Shikder Tarek <shikdertarek2019@gmail.com>
2024-06-21 23:05:49 +0600

    Add paginationon manage register camp

--- src/Pages/Dashboard/ManageRegisterCamp.jsx
+++ src/Pages/Dashboard/ManageRegisterCamp.jsx
@@ -4,14 +4,44 @@ import useAxiosSecure from "../../Hooks/useAxiosSecure";
 import { MdCancel, MdFileDownloadDone } from "react-icons/md";
 import Swal from "sweetalert2";
 import { Helmet } from "react-helmet-async";
+import { useState } from "react";
 
 const ManageRegisterCamp = () => {
     const {user} = useAuth()
     const axiosSecure = useAxiosSecure()
+    const [currentPage, setCurrentPage] = useState(0);
+
+
+    // Pagination
+    const {data:itemCount=[]} = useQuery({
+      queryKey: ['participantCampCount'],
+      queryFn: async() =>{
+          const res = await axiosSecure.get('/participantCampCount');
+          return res.data.count;
+      }
+  })
+
+  const numberOfPage = Math.ceil(itemCount / 10)
+    const pages = [...Array(numberOfPage).keys()];
+
+    const handlePrevPage = () => {
+        if (currentPage > 0) {
+            setCurrentPage(currentPage - 1);
+        }
+    }
+
+    const handleNextPage = () => {
+        if (currentPage < pages.length - 1) {
+            setCurrentPage(currentPage + 1);
+        }
+    }
+
+
+
     const { data:registerCamp=[], isLoading, refetch } = useQuery({
-      queryKey: ['participant', user.email],
+      queryKey: ['participant', user.email, currentPage],
       queryFn: async () => {
-          const res = await axiosSecure.get(`participant_camp`);
+          const res = await axiosSecure.get(`participant_camp?page=${currentPage}&size=${10}`);
           return res.data; 
       }
   });
@@ -145,6 +175,28 @@ const ManageRegisterCamp = () => {
    
   </table>
 </div>
+{/* Pagination */}
+<div className="flex justify-center mt-12">
+            <div aria-label="Pagination" className="inline-flex -space-x-px rounded-md shadow-sm bg-gray-100 text-gray-800">
+	<button onClick={handlePrevPage}  className="inline-flex items-center px-2 py-2 text-sm font-semibold border rounded-l-md border-gray-300">
+		<span className="sr-only">Previous</span>
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" className="w-5 h-5">
+			<path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd"></path>
+		</svg>
+	</button>
+	{
+        pages.map(page=> <button key={page} onClick={()=>setCurrentPage(page)} className={`inline-flex items-center px-4 py-2 text-sm font-semibold border border-gray-300 ${currentPage === page ? 'bg-violet-600 text-white' : 'bg-blue-100'}`}>{page}</button>)
+    }
+	
+	<button onClick={handleNextPage} className="inline-flex items-center px-2 py-2 text-sm font-semibold border rounded-r-md border-gray-300">
+		<span className="sr-only">Next</span>
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" className="w-5 h-5">
+			<path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path>
+		</svg>
+	</button>
+</div>
+            </div>
+{/* Pagination end */}
         </div>
     );
 };
